473a26ae62ac8880f3489f9ca97d5ae2021b1630
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2021-04-26 10:37:23 +0300

    Kill user program - multiple programs and signals

--- user/kill.c
+++ user/kill.c
@@ -5,14 +5,20 @@
 int
 main(int argc, char **argv)
 {
-  if(argc != 3){
-    fprintf(2, "usage: kill <pid> <signal>\n");
+  if(argc < 3) {
+    fprintf(2, "usage: kill (<pid> <signal>)+\n");
     exit(1);
   }
-  int pid = atoi(argv[1]);
-  int signum = atoi(argv[2]);
-  if (kill(pid, signum) < 0) {
-    printf("sending signal %d to process %d failed.\n", signum, pid);
+  if (argc % 2 == 0) {
+    fprintf(2, "expected even number of arguments.\nusage: kill (<pid> <signal>)+\n");
+    exit(2);
+  }
+  for (int i = 1; i < argc; i += 2) {
+    int pid = atoi(argv[i]);
+    int signum = atoi(argv[i + 1]);
+    if (kill(pid, signum) < 0) {
+      printf("iter %d: sending signal %d to process %d failed.\n", (i / 2) + 1, signum, pid);
+    }
   }
   exit(0);
 }
