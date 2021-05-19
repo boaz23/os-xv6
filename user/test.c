4f85c39b542d421377a598d454ae9ac5e806397f
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2021-05-19 20:53:21 +0300

    Roee test prints

--- user/test.c
+++ user/test.c
@@ -5,27 +5,30 @@
 #define PGSIZE 4096
 #define FREE_SPACE_ON_RAM 12
 #define PG_AMOUNT 26
+#define ADDR(i) (i * PGSIZE + 1)
+
 int main()
 {
+    printf("parent pid %d\n", getpid());
     printf("here\n");
     char *alloc = malloc(PG_AMOUNT * PGSIZE);
-    printf("Allocated\n");
+    printf("Allocated %p\n", alloc);
     for (int i = 0; i < PG_AMOUNT; i++)
     {
-        alloc[i * PGSIZE + 1] = 'a' + i;
-        printf("%d\n", i);
+        printf("%d, %p\n", i, &alloc[ADDR(i)]);
+        alloc[ADDR(i)] = 'a' + i;
     }
     for (int i = 0; i < PG_AMOUNT; i++)
     {
-        printf("alloc[%d] = %c\n", i * PGSIZE, alloc[i * PGSIZE + 1]);
+        printf("alloc[%p] = %c\n", i * PGSIZE, alloc[ADDR(i)]);
     }
     int pid = fork();
     if (pid == 0)
     {
-        printf("child printing:\n");
+        printf("child %d printing:\n", getpid());
         for (int i = 0; i < 20; i++)
         {
-            printf("    alloc[%d] = %c\n", i * PGSIZE, alloc[i * PGSIZE]);
+            printf("    alloc[%p] = %c\n", i * PGSIZE, alloc[ADDR(i)]);
         }
     }
     else if(pid > 0){
