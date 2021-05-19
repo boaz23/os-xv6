54eb54da6b1ec6d627a65737af501c87dd3577d8
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2021-05-19 16:06:01 +0300

    Roee test changes
    * Added it to makefile
    * Enabled child printing

--- user/test.c
+++ user/test.c
@@ -4,28 +4,29 @@
 
 #define PGSIZE 4096
 #define FREE_SPACE_ON_RAM 12
+#define PG_AMOUNT 26
 int main()
 {
     printf("here\n");
-    char *alloc = malloc(20 * PGSIZE);
+    char *alloc = malloc(PG_AMOUNT * PGSIZE);
     printf("Allocated\n");
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < PG_AMOUNT; i++)
     {
-        alloc[i * PGSIZE] = 'a' + i;
+        alloc[i * PGSIZE + 1] = 'a' + i;
         printf("%d\n", i);
     }
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < PG_AMOUNT; i++)
     {
-        printf("alloc[%d] = %c\n", i * PGSIZE, alloc[i * PGSIZE]);
+        printf("alloc[%d] = %c\n", i * PGSIZE, alloc[i * PGSIZE + 1]);
     }
     int pid = fork();
     if (pid == 0)
     {
         printf("child printing:\n");
-        // for (int i = 0; i < 20; i++)
-        // {
-        //     printf("    alloc[%d] = %c\n", i * PGSIZE, alloc[i * PGSIZE]);
-        // }
+        for (int i = 0; i < 20; i++)
+        {
+            printf("    alloc[%d] = %c\n", i * PGSIZE, alloc[i * PGSIZE]);
+        }
     }
     else if(pid > 0){
         wait(0);
