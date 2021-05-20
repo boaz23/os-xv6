c0ae497a4fb6cb2391f0ac5d64c69eb5cf6d9ea0
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2021-05-20 11:07:57 +0300

    Test - segmentation fault

--- user/test.c
+++ user/test.c
@@ -4,14 +4,15 @@
 
 #define PGSIZE 4096
 #define FREE_SPACE_ON_RAM 12
-#define PG_AMOUNT 20
+#define PG_AMOUNT 0x14
 #define ADDR(i) (i * PGSIZE)
 
 int main()
 {
+    // char c;
     printf("parent pid %d\n", getpid());
-    char *alloc = malloc(PG_AMOUNT * PGSIZE);
-    printf("Allocated %p\n", alloc);
+    char *alloc = sbrk(PG_AMOUNT * PGSIZE);
+    printf("Allocated %p, %p\n", alloc, sbrk(0));
     for (int i = 0; i < PG_AMOUNT; i++)
     {
         printf("%d, %p\n", i, &alloc[ADDR(i)]);
@@ -29,9 +30,12 @@ int main()
         {
             printf("    alloc[%p] = %c\n", i * PGSIZE, alloc[ADDR(i)]);
         }
+        // alloc[PG_AMOUNT*PGSIZE] = 5;
     }
     else if(pid > 0){
         wait(0);
+        // c = alloc[PG_AMOUNT*PGSIZE];
+        // printf("c: %d\n", c);
     }
     free(alloc);
     exit(0);
